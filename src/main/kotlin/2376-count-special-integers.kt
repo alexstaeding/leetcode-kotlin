483a4b0163b2828c9282cd908dd67422d8a3b4a9
Alexander Städing <alexanderstaeding@gmail.com>
2023-02-18 22:24:00 +0100

    2376: optimize more

--- src/main/kotlin/2376-count-special-integers.kt
+++ src/main/kotlin/2376-count-special-integers.kt
@@ -5,12 +5,21 @@ fun main() {
 
 fun calculate(prefix: Int, max: Int): Int {
     var total = 0
-    for (i in 0..9) {
-        if (!prefix.toString().contains(i.toString())) {
-            val newNum = prefix * 10 + i
-            if (newNum <= max) {
-                total += calculate(newNum, max) + 1
+    outer@ for (i in 0..9) {
+        // check if i is already in prefix
+        var p = prefix
+        while (p >= 0) {
+            if (p % 10 == i) {
+                continue@outer
             }
+            p /= 10
+            if (p == 0) {
+                break
+            }
+        }
+        val newNum = prefix * 10 + i
+        if (newNum <= max) {
+            total += calculate(newNum, max) + 1
         }
     }
     return total
