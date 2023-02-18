c988fa3760f0f826342a507af1741e6595c8c3aa
Alexander Städing <alexanderstaeding@gmail.com>
2023-02-18 22:12:30 +0100

    2376: remove sequence

--- src/main/kotlin/2376-count-special-integers.kt
+++ src/main/kotlin/2376-count-special-integers.kt
@@ -1,18 +1,17 @@
 fun main() {
     val max = 20
-    println(calculate(0, max).count())
+    println(calculate(0, max))
 }
 
-fun calculate(prefix: Int, max: Int): Sequence<Int> {
-    return sequence {
-        for (i in 0..9) {
-            if (!prefix.toString().contains(i.toString())) {
-                val newNum = prefix * 10 + i
-                if (newNum <= max) {
-                    yield(newNum)
-                    yieldAll(calculate(newNum, max))
-                }
+fun calculate(prefix: Int, max: Int): Int {
+    var total = 0
+    for (i in 0..9) {
+        if (!prefix.toString().contains(i.toString())) {
+            val newNum = prefix * 10 + i
+            if (newNum <= max) {
+                total += calculate(newNum, max) + 1
             }
         }
     }
+    return total
 }
