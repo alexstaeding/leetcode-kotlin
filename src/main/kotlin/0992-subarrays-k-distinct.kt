6643c293386733d509ea40719ccdd4f424f860bb
Alexander Städing <alexanderstaeding@gmail.com>
2023-08-27 00:16:25 +0200

    0992: Fix more edge cases

--- src/main/kotlin/0992-subarrays-k-distinct.kt
+++ src/main/kotlin/0992-subarrays-k-distinct.kt
@@ -33,7 +33,7 @@ private fun subarraysWithKDistinct(nums: IntArray, k: Int): Int {
             } else {
                 println()
             }
-            if (low + k - 1 == high) {
+            if (low + k - 2 == high) {
                 high++
             }
             continue
@@ -48,20 +48,11 @@ private fun subarraysWithKDistinct(nums: IntArray, k: Int): Int {
                 result++
                 println("found!")
             } else {
+                high++
                 println()
             }
-            high++
             continue
         }
-        print("[$low, $high]")
-        // if distinct == k
-        if (inc(nums[high]) == 1) { // completely removed number
-            distinct++
-            println(" = ${nums.drop(low).take(high - low + 1)} $map :: distinct($distinct) result($result)")
-            continue
-        }
-        println(" = ${nums.drop(low).take(high - low + 1)} $map :: distinct($distinct) result($result) found! 1")
-        result++
         if (high + 1 == nums.size) {
             println("End of ze line")
             // we have reached the end of the array
@@ -84,7 +75,7 @@ private fun subarraysWithKDistinct(nums: IntArray, k: Int): Int {
             break
         }
 
-        if (checkInc(nums[high + 1]) == 1) {
+        if (low < high && checkInc(nums[high + 1]) == 1) {
             println("Next is bad")
             // the next number would increment the distinct number
             // go back and increment low by one and set high = low + k - 1 (the minimum interval size)
@@ -106,10 +97,19 @@ private fun subarraysWithKDistinct(nums: IntArray, k: Int): Int {
 
             assert(low + k - 1 == high)
         }
+        high++
+        print("[$low, $high]")
+        // if distinct == k
+        if (inc(nums[high]) == 1) { // completely removed number
+            distinct++
+            println(" = ${nums.drop(low).take(high - low + 1)} $map :: distinct($distinct) result($result)")
+            continue
+        }
+        println(" = ${nums.drop(low).take(high - low + 1)} $map :: distinct($distinct) result($result) found! 1")
+        result++
         if (low > nums.size) {
             break
         }
-        high++
     }
     return result
 }
