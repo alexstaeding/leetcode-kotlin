d350d99b8d8c71f147c7cdbb8ce2450f22501ab8
Alexander Städing <alexanderstaeding@gmail.com>
2023-09-12 22:10:21 +0200

    phisn-question-1: First attempt

---
+++ src/main/kotlin/phisn-question-1.kt
@@ -0,0 +1,56 @@
+import kotlin.math.max
+
+fun notIncreasingFromTwo(a: IntArray, b: IntArray): Int {
+    assert(a.size == b.size)
+    assert(a.isNotEmpty())
+    var current = 1
+    val processingIntervals = mutableMapOf<Int, MutableList<Int>>()
+    val finishedIntervals = mutableMapOf<Int, MutableList<Int>>()
+
+    while (current < a.size) {
+        processingIntervals.forEach { (first, list) ->
+            val last = list.last()
+            if (a[current] > last && b[current] > last) {
+                finishedIntervals[first] = list
+            }
+        }
+        finishedIntervals.forEach { (k, _) -> processingIntervals.remove(k) }
+
+        if (a[current] <= a[current - 1] || b[current] <= b[current - 1]) {
+            processingIntervals.computeIfAbsent(current - 1) { _ ->
+                mutableListOf(
+                    max(a[current - 1], b[current - 1])
+                )
+            }
+            val aValid = a[current] <= a[current - 1] || a[current] <= b[current - 1]
+            val bValid = b[current] <= b[current - 1] || b[current] <= a[current - 1]
+            processingIntervals.forEach { (_, list) ->
+                if (aValid && !bValid && a[current] <= list.last()) {
+                    list += a[current]
+                } else if (bValid && !aValid && b[current] <= list.last()) {
+                    list += b[current]
+                } else if (max(a[current], b[current]) <= list.last()) {
+                    // both valid
+                    list += max(a[current], b[current])
+                }
+            }
+        }
+
+        current++
+        if (current == a.size) {
+            processingIntervals.forEach { (k, v) -> finishedIntervals[k] = v }
+        }
+    }
+    println("Intervals:\n${finishedIntervals.toList().joinToString("\n")}")
+    return finishedIntervals.maxOf { (_, b) -> b.size }
+}
+
+fun main() {
+    val a = intArrayOf(1, 4, 3, 4, 9)
+    val b = intArrayOf(1, 1, 2, 2, 10)
+    println(notIncreasingFromTwo(a, b))
+
+    val c = intArrayOf(1, 3, 5, 1, 4, 4, 3, 3)
+    val d = intArrayOf(0, 2, 2, 5, 0, 8, 3, 3)
+    println(notIncreasingFromTwo(c, d))
+}
