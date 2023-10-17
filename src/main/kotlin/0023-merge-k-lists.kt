3c78a3da91e2761b2ebf6c5b8d9fda539da15b4d
Alexander Städing <alexanderstaeding@gmail.com>
2023-10-17 19:28:44 +0200

    0023: Use a priority queue, improve performance ~2.5x

--- src/main/kotlin/0023-merge-k-lists.kt
+++ src/main/kotlin/0023-merge-k-lists.kt
@@ -1,23 +1,23 @@
+import java.util.Comparator
+import java.util.PriorityQueue
+
 fun mergeKLists(lists: Array<ListNode?>): ListNode? {
     if (lists.all { it == null }) return null
     // at least one element is not empty
     var head: ListNode? = null
     var tail: ListNode? = null
     // save position of each sub-list
-    val positions = lists.asSequence()
-        .filterNotNull()
-        .withIndex()
-        .associateTo(mutableMapOf()) { it.index to it.value }
-    OUTER@ while (true) {
-        val listNode = positions.minByOrNull { (_, value) -> value.`val` }
+    val positions = lists.filterNotNullTo(PriorityQueue(lists.size * 20, Comparator.comparing(ListNode::`val`)))
+    while (true) {
+        val listNode = positions.poll()
             ?: return head
         if (head == null || tail == null) {
-            head = listNode.value
+            head = listNode
             tail = head
         } else {
-            tail.next = listNode.value
+            tail.next = listNode
             tail = tail.next
         }
-        positions.compute(listNode.key) { _, v -> v?.next }
+        listNode.next?.let { positions.add(it) }
     }
 }
