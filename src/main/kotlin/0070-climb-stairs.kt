d7f225459eb18b6b4bcf62ca8ffcb3923fbec29d
Alexander Städing <alexanderstaeding@gmail.com>
2023-11-03 23:17:55 +0100

    0070: dYnAmIc PrOGrAmMiNg

--- src/main/kotlin/0070-climb-stairs.kt
+++ src/main/kotlin/0070-climb-stairs.kt
@@ -1,19 +1,28 @@
 import java.math.BigInteger
 
+val factorialCache = HashMap<Int, BigInteger>(200)
+val binomCache = HashMap<Int, BigInteger>(200)
+
 fun factorial(n: Int, downTo: Int = 2): BigInteger {
+    factorialCache[n + (downTo shl 16)]?.let {
+        return it
+    }
     var total = BigInteger.ONE
     for (i in n downTo downTo) {
         total = total.multiply(BigInteger.valueOf(i.toLong()))
     }
-    return total
+    return total.also { factorialCache[n + (downTo shl 16)] = it }
 }
 
 fun binomCoeff(n: Int, k: Int): BigInteger {
+    binomCache[n + (k shl 16)]?.let {
+        return it
+    }
     return if (k > n / 2) {
         factorial(n, k + 1) / factorial(n - k)
     } else {
         factorial(n, (n - k) + 1) / factorial(k)
-    }
+    }.also { binomCache[n + (k shl 16)] = it }
 }
 
 fun climbStairs(n: Int): Int {
