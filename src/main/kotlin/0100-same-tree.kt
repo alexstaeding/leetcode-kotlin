0e2bc6985fc1a09b6872a682997b283dd3b4f600
Alexander Städing <alexanderstaeding@gmail.com>
2024-01-19 18:27:18 +0100

    0100: Clean up

--- src/main/kotlin/0100-same-tree.kt
+++ src/main/kotlin/0100-same-tree.kt
@@ -1,23 +1,6 @@
 fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
-    if ((p == null) != (q == null)) {
-        return false
-    }
-
-    if (p?.`val` != q?.`val`) {
-        return false
-    }
-
-    if (p?.left != null || q?.left != null) {
-        if (!isSameTree(p?.left, q?.left)) {
-            return false
-        }
-    }
-
-    if (p?.right != null || q?.right != null) {
-        if (!isSameTree(p?.right, q?.right)) {
-            return false
-        }
-    }
-
-    return true
+    return (p == null) == (q == null)
+        && p?.`val` == q?.`val`
+        && !((p?.left != null || q?.left != null) && !isSameTree(p?.left, q?.left))
+        && !((p?.right != null || q?.right != null) && !isSameTree(p?.right, q?.right))
 }
