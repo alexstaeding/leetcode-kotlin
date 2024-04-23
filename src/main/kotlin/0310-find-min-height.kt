f9da356d662e5b37e0685a5f73828f7e3f5278ec
Alexander Städing <alexanderstaeding@gmail.com>
2024-04-23 15:42:01 +0200

    0310: Small cleanup

--- src/main/kotlin/0310-find-min-height.kt
+++ src/main/kotlin/0310-find-min-height.kt
@@ -3,8 +3,6 @@ fun findMinHeightTrees(n: Int, edges: Array<IntArray>): List<Int> {
         return listOf(0)
     }
 
-    class Node(val id: Int, val depth: Int = 0)
-
     val lookup = Array(n) { mutableSetOf<Int>() }
 
     for ((a, b) in edges) {
@@ -12,39 +10,31 @@ fun findMinHeightTrees(n: Int, edges: Array<IntArray>): List<Int> {
         lookup[b].add(a)
     }
 
-    // prev, node -> min height
-    class CacheKey(val prev: Int, val next: Int) {
+    data class Arguments(val prev: Int, val next: Int) {
         override fun hashCode(): Int = prev shl 16 or next
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
-            if (other !is CacheKey) return false
+            if (other !is Arguments) return false
             return prev == other.prev && next == other.next
         }
     }
-    val cache = HashMap<CacheKey, Int>(n*2)
 
-    data class Arguments(val prev: Node, val next: Int)
+    // prev, node -> min height
+    val cache = HashMap<Arguments, Int>(n*2)
 
-    var outerCalls = 0
-    var innerCalls = 0
-
-    val getDepth = DeepRecursiveFunction<Arguments, Int> { (prev, next) ->
-        println(" ".repeat(prev.depth) + "> Entering getDepth(${prev.id}, $next)")
-        outerCalls++
-        cache.getOrPut(CacheKey(prev.id, next)) {
-            println(" ".repeat(prev.depth) + "> calculating(${prev.id}, $next)")
-            innerCalls++
-            1 + (lookup[next].filter { it != prev.id }.maxOfOrNull { callRecursive(Arguments(Node(next, prev.depth + 1), it)) } ?: 0)
+    val getDepth = DeepRecursiveFunction<Arguments, Int> { args ->
+        cache.getOrPut(args) {
+            1 + (lookup[args.next].filter { it != args.prev }.maxOfOrNull { callRecursive(Arguments(args.next, it)) } ?: 0)
         }
     }
 
-    fun Node.getDepth(): Int = lookup[id].maxOf { getDepth(Arguments(this, it)) } + 1
+    fun getDepth(id: Int): Int = lookup[id].maxOf { getDepth(Arguments(id, it)) } + 1
 
     var minTreeSize = n
     val roots = mutableListOf<Int>()
 
     for (id in 0..<n) {
-        val depth = Node(id).getDepth()
+        val depth = getDepth(id)
         if (depth < minTreeSize) {
             minTreeSize = depth
             roots.clear()
@@ -53,8 +43,6 @@ fun findMinHeightTrees(n: Int, edges: Array<IntArray>): List<Int> {
             roots.add(id)
         }
     }
-
-    println("nodes = $n, outerCalls = $outerCalls, innerCalls = $innerCalls, cacheSize = ${cache.size}")
 
     return roots
 }
