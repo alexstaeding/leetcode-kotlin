2ee8810cfb32f5dcc68dcb8536c9229b0ad633bd
Alexander Städing <alexanderstaeding@gmail.com>
2024-04-23 23:27:27 +0200

    0310: Update approach (thanks lenny)

--- src/main/kotlin/0310-find-min-height.kt
+++ src/main/kotlin/0310-find-min-height.kt
@@ -3,46 +3,34 @@ fun findMinHeightTrees(n: Int, edges: Array<IntArray>): List<Int> {
         return listOf(0)
     }
 
-    val lookup = Array(n) { mutableSetOf<Int>() }
+    val lookup = (0..<n).associateWithTo(LinkedHashMap()) { mutableSetOf<Int>() }
 
-    for ((a, b) in edges) {
-        lookup[a].add(b)
-        lookup[b].add(a)
+    for ((l, r) in edges) {
+        lookup[l]!!.add(r)
+        lookup[r]!!.add(l)
     }
 
-    data class Arguments(val prev: Int, val next: Int) {
-        override fun hashCode(): Int = prev shl 16 or next
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (other !is Arguments) return false
-            return prev == other.prev && next == other.next
+    var updateEdges = (0..<n).toSet()
+    var lastUpdateEdges = updateEdges
+
+    while (updateEdges.isNotEmpty()) {
+        val singleEdges = mutableListOf<Pair<Int, Int>>()
+        for (key in updateEdges) {
+            lookup[key]?.let { con ->
+                if (con.size == 1) {
+                    singleEdges.add(key to con.first())
+                }
+            }
+        }
+        lastUpdateEdges = updateEdges
+        updateEdges = mutableSetOf()
+
+        for ((key, con) in singleEdges) {
+            lookup.remove(key)
+            lookup[con]?.remove(key)
+            updateEdges.add(con)
         }
     }
 
-    // prev, node -> min height
-    val cache = HashMap<Arguments, Int>(n*2)
-
-    val getDepth = DeepRecursiveFunction<Arguments, Int> { args ->
-        cache.getOrPut(args) {
-            1 + (lookup[args.next].filter { it != args.prev }.maxOfOrNull { callRecursive(Arguments(args.next, it)) } ?: 0)
-        }
-    }
-
-    fun getDepth(id: Int): Int = lookup[id].maxOf { getDepth(Arguments(id, it)) } + 1
-
-    var minTreeSize = n
-    val roots = mutableListOf<Int>()
-
-    for (id in 0..<n) {
-        val depth = getDepth(id)
-        if (depth < minTreeSize) {
-            minTreeSize = depth
-            roots.clear()
-            roots.add(id)
-        } else if (depth == minTreeSize) {
-            roots.add(id)
-        }
-    }
-
-    return roots
+    return lastUpdateEdges.sorted()
 }
