b6b0419566955b9ae65114591ec4ad9437aca280
Alexander Städing <alexanderstaeding@gmail.com>
2024-05-07 18:34:25 +0200

    2816: Add more test cases

--- src/test/kotlin/2816-double-it-test.kt
+++ src/test/kotlin/2816-double-it-test.kt
@@ -5,4 +5,13 @@ class DoubleItTest : FunSpec({
     test("doubleIt([1, 8, 9])") {
         doubleIt(listOf(1, 8, 9).toListNodes()) shouldBe listOf(3, 7, 8).toListNodes()
     }
+    test("doubleIt([9, 9, 9])") {
+        doubleIt(listOf(9, 9, 9).toListNodes()) shouldBe listOf(1, 9, 9, 8).toListNodes()
+    }
+    test("doubleIt([1])") {
+        doubleIt(listOf(1).toListNodes()) shouldBe listOf(2).toListNodes()
+    }
+    test("doubleIt([])") {
+        doubleIt(emptyList<Int>().toListNodes()) shouldBe null
+    }
 })
